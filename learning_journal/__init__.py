af6438f2b17a95bd7583d5c9b1efbf5a476abbf5
Rick Valenzuela <devnull@rickv.com>
2017-01-03 19:27:24 -0800

    made DATABASE_URL env for Heroku

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -6,6 +6,7 @@ from os import environ
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application."""
 #    settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
+    settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
