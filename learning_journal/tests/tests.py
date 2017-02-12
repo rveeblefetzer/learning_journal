bab41ea72ad3de366366a6df9ca48c6c17ed462c
Rick Valenzuela <devnull@rickv.com>
2017-02-12 09:33:09 -0800

    moved tests.py to tests folder

--- learning_journal/tests/tests.py
+++ learning_journal/tests/tests.py
@@ -1,65 +1,108 @@
-import unittest
+import pytest
 import transaction
-
+import time
 from pyramid import testing
 
+from learning_journal.models import (
+    Entry,
+    get_tm_session,
+)
+from learning_journal.models.meta import Base
+from learning_journal.scripts.initializedb import ENTRIES
 
-def dummy_request(dbsession):
-    return testing.DummyRequest(dbsession=dbsession)
+MODEL_ENTRIES = [Entry(
+    title=entry['title'],
+    body=entry['body'],
+    creation_date=entry['creation_date']
+) for entry in ENTRIES]
 
+@pytest.fixture(scope="session")
+def configuration(request):
+    """Set up a Configurator instance.
 
-class BaseTest(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp(settings={
-            'sqlalchemy.url': 'sqlite:///:memory:'
-        })
-        self.config.include('.models')
-        settings = self.config.get_settings()
+    This Configurator instance sets up a pointer to the location of the
+        database.
+    It also includes the models from your app's model package.
+    Finally it tears everything down, including the in-memory SQLite database.
 
-        from .models import (
-            get_engine,
-            get_session_factory,
-            get_tm_session,
-            )
+    This configuration will persist for the entire duration of your PyTest run.
+    """
+    config = testing.setUp(settings={
+        'sqlalchemy.url': 'sqlite:///:memory:'
+    })
+    config.include(".models")
 
-        self.engine = get_engine(settings)
-        session_factory = get_session_factory(self.engine)
-
-        self.session = get_tm_session(session_factory, transaction.manager)
-
-    def init_database(self):
-        from .models.meta import Base
-        Base.metadata.create_all(self.engine)
-
-    def tearDown(self):
-        from .models.meta import Base
-
+    def teardown():
         testing.tearDown()
-        transaction.abort()
-        Base.metadata.drop_all(self.engine)
+
+    request.addfinalizer(teardown)
+    return config
 
 
-class TestMyViewSuccessCondition(BaseTest):
+@pytest.fixture(scope="function")
+def db_session(configuration, request):
+    """Create a session for interacting with the test database.
 
-    def setUp(self):
-        super(TestMyViewSuccessCondition, self).setUp()
-        self.init_database()
+    This uses the dbsession_factory on the configurator instance to create a
+    new database session. It binds that session to the available engine
+    and returns a new session for every call of the dummy_request object.
+    """
+    SessionFactory = configuration.registry["dbsession_factory"]
+    session = SessionFactory()
+    engine = session.bind
+    Base.metadata.create_all(engine)
 
-        from .models import MyModel
+    def teardown():
+        session.transaction.rollback()
 
-        model = MyModel(name='one', value=55)
-        self.session.add(model)
-
-    def test_passing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'learning_journal')
+    request.addfinalizer(teardown)
+    return session
 
 
-class TestMyViewFailureCondition(BaseTest):
+@pytest.fixture
+def dummy_request(db_session, method="GET"):
+    """Instantitate an HTTP request and database session for testing"""
+    request = testing.DummyRequest()
+    request.method = method
+    request.dbsession = db_session
+    return request
 
-    def test_failing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info.status_int, 500)
+
+def test_database_empty_but_exists(db_session):
+    """Test that app connects to database, which is empty here."""
+    assert len(db_session.query(Entry).all()) == 0
+
+
+def test_my_view(dummy_request):
+    """Test the homepage view returns data from database."""
+    from .views.default import my_view
+    dummy_request.dbsession.add(Entry(title="one", id='1'))
+    result = my_view(dummy_request) # views commit changes to the DB
+    assert result["entries"][0].title == "one"
+
+
+def test_write_view(dummy_request, db_session):
+    """Test the write view writes to database."""
+    from .views.default import write
+    dummy_request.method = "POST"
+    dummy_request.POST["title"] = "This is a Title"
+    dummy_request.POST["body"] = "And this is the body. TEST!"
+
+    write(dummy_request)
+    query = db_session.query(Entry).all()
+    assert query[0].title == "This is a Title"
+    assert query[0].body == "And this is the body. TEST!"
+
+
+def test_detail_view(dummy_request, db_session):
+    """Test the detail view."""
+    from .views.default import detail
+    dummy_request.method = "POST"
+    dummy_request.POST["title"] = "This is a Title"
+    dummy_request.POST["body"] = "And this is the body. TEST!"
+    dummy_request.matchdict['id'] = '1'
+    result = detail(dummy_request)
+    entry = dummy_request.dbsession.query(Entry).get(1)
+    assert result['entry'] == entry
+
+
